94dc8b6f1d200e741499d8ba43382d542a47d228
chihiro-kakizaki <kkzkchr@gmail.com>
2022-03-18 17:21:31 +0900

    s3設定

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,7 +4,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  #storage :file
+  storage :fog
   process resize_to_limit: [1000, 1000]
 
   # Override the directory where uploaded files will be stored.
