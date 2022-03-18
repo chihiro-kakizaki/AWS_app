3b9afe483737aa6f21d3e8f218e8e5d2568b8853
chihiro-kakizaki <kkzkchr@gmail.com>
2022-03-18 18:11:50 +0900

    fix:デプロイ先の記述修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :repo_url, 'https://github.com/chihiro-kakizaki/AWS_app'
 set :branch, ENV['BRANCH'] || 'master'
 
 # deploy先のディレクトリ。
-set :deploy_to, '/var/www/achieve'
+set :deploy_to, '/var/www/AWS_app'
 
 # シンボリックリンクをはるフォルダ・ファイル
 set :linked_files, %w{.env config/secrets.yml}
